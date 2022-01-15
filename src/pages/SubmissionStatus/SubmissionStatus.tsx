5f73353a51235f851e1e32f1061dd44fe38ed835
coupacoupa <44894563+coupacoupa@users.noreply.github.com>
2022-01-15 18:41:53 +0800

    Used api data for view submission page

--- src/pages/SubmissionStatus/SubmissionStatus.tsx
+++ src/pages/SubmissionStatus/SubmissionStatus.tsx
@@ -6,44 +6,9 @@ import Accordion from './components/Accordion';
 const SubmissionStatus = () => {
   const { isLoading, isError, data, error } = useGetAllUserSubmission('email@email.com', '12345');
 
-  const getSubmissionStatus = (): ISubmissionStatus[] => {
-    return [
-      {
-        name: 'name',
-        email: 'email',
-        agencyName: 'agencyName',
-        contactNumber: '123123',
-        createdDate: '11/1/2022',
-        feedback: 'this is a  very long and detailed feedback',
-        feedbackStatus: 'Accepted',
-        submissionNo: 1,
-      },
-      {
-        name: 'name',
-        email: 'email',
-        agencyName: 'agencyName',
-        contactNumber: '123123',
-        createdDate: '11/3/2022',
-        feedback: 'this is a  very long and detailed feedback',
-        feedbackStatus: 'Rejected',
-        submissionNo: 2,
-      },
-      {
-        name: 'name',
-        email: 'email',
-        agencyName: 'agencyName',
-        contactNumber: '123123',
-        createdDate: '5/1/2021',
-        feedback: 'this is a  very long and detailed feedback',
-        feedbackStatus: 'Accepted',
-        submissionNo: 3,
-      },
-    ];
-  };
-
   return (
     <div>
-      <Accordion submissionStatus={getSubmissionStatus()} />
+      <Accordion submissionStatus={data} />
     </div>
   );
 };
